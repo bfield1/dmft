144006ec61b9735bda03743df03e0d92a151e2bb
bfield1 <bernard.field1@monash.edu>
2022-01-31 17:11:12 +1100

    Script to generate DOS from a given dispersion

---
+++ python_modules/dmft/dos.py
@@ -0,0 +1,60 @@
+"""
+Generates the density of states.
+"""
+
+import numpy as np
+
+def dos_from_band(dispersion, nk, dims=2, bins=None, de=None):
+    """
+    Creates discrete DOS from a continuous band dispersion
+
+    Histograms band energies from a discrete k-grid
+
+    Inputs:
+        dispersion - f(kx, ky,...) -> float. Band dispersion, takes numpy
+            arrays for the momenta in fractional coordinates [0,1).
+        nk - positive integer or tuple of integers. K-point grid to use.
+        dims - positive integer. Number of dimensions.
+        bins - bins argument for np.histogram. Number of energy points.
+        de - positive float, used if bins is not provided. Width of energy
+            bins.
+    Outputs: Several numpy arrays, shape (number_of_bins,).
+        rho - density of states
+        energy - midpoints of energy bins
+        delta - width of energy bins
+    DOS is normalised such that np.sum(rho*delta) == 1.
+    """
+    # Check that we have a bins or de argument
+    if bins is None and de is None:
+        raise TypeError("Must provide one of bins or de")
+    # Tuple-fy nk
+    try:
+        len(nk)
+    except TypeError:
+        nk = (nk,)*dims
+    else:
+        if len(nk) != dims:
+            raise ValueError("nk's length does not match dims")
+    # Create a k-point grid
+    ks = [np.linspace(0, 1, n) for n in nk]
+    ks = np.meshgrid(*ks)
+    # Produce a dispersion
+    energies = dispersion(*ks)
+    # Determine how many bins we need
+    if bins is None:
+        # Number of bins needed
+        bins = int(np.ceil((energies.max() - energies.min())/de))
+        # Now we expand the energy range so the bins are of the right width,
+        # keeping the midpoint the same.
+        emin = (energies.max() + energies.min() - bins*de)/2
+        emax = bins*de + emin
+    else:
+        emin = energies.min()
+        emax = energies.max()
+    # Histogram it
+    rho, bin_edges = np.histogram(energies, bins, range=(emin,emax), density=True)
+    # Energy differences
+    delta = np.diff(bin_edges)
+    # Bin midpoints
+    energy = (bin_edges[0:-1] + bin_edges[1:])/2
+    return rho, energy, delta
