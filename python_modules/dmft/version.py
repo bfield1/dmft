74d35405f0e74219b45ffc08bda671440c06d7de
bfield1 <bernard.field1@monash.edu>
2022-02-04 12:52:14 +1100

    Module for getting current git version

---
+++ python_modules/dmft/version.py
@@ -0,0 +1,18 @@
+"""
+Return the current git hash
+"""
+
+import os.path
+import subprocess
+
+import dmft.version
+
+def get_git_hash():
+    # We need to spawn a subprocess, which goes to the source directory, then
+    # uses git to read the git hash
+    dirname = os.path.dirname(dmft.version.__file__)
+    output = subprocess.run(['git','rev-parse','HEAD'], cwd=dirname, check=True, text=True, capture_output=True).stdout
+    return output.rstrip('\n')
+
+if __name__ == "__main__":
+    print(get_git_hash())
