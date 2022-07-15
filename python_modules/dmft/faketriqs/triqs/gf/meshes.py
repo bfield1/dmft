1471a2d72df34c8cdbd28f88323e329a04c3bb70
bfield1 <bernard.field1@monash.edu>
2022-07-15 13:54:11 +1000

    Copy method for meshes

--- python_modules/dmft/faketriqs/triqs/gf/meshes.py
+++ python_modules/dmft/faketriqs/triqs/gf/meshes.py
@@ -6,6 +6,9 @@ from .mesh_point import MeshValueGenerator, MeshPoint
 statistic_enum = ['Boson','Fermion']
 
 class Mesh_Generic():
+    """
+    A linear mesh between two values
+    """
     def __init__(self, x_min, x_max, n):
         self.x_min = x_min
         self.x_max = x_max
@@ -27,6 +30,10 @@ class Mesh_Generic():
         return self.n
     def values(self):
         return MeshValueGenerator(self)
+    def __eq__(self, other):
+        return (self.x_min == other.x_min) and (self.x_max == other.x_max) and (self.n == other.n)
+    def __ne__(self, other):
+        return not self == other
 
 class MeshImTime(Mesh_Generic):
     def __init__(self, beta, S, n_max):
@@ -36,14 +43,20 @@ class MeshImTime(Mesh_Generic):
         else:
             raise ValueError("S must be 'Boson' or 'Fermion'.")
         super().__init__(x_min=0, x_max=beta, n=n_max)
+    def copy(self):
+        return MeshImTime(self.beta, self.statistic, self.n)
 
 class MeshReFreq(Mesh_Generic):
     def __init__(self, omega_min, omega_max, n_max):
         super().__init__(x_min=omega_min, x_max=omega_max, n=n_max)
+    def copy(self):
+        return MeshReFreq(self.x_min, self.x_max, self.n)
 
 class MeshReTime(Mesh_Generic):
     def __init__(self, t_min, t_max, n_max):
         super().__init__(x_min=t_min, x_max=t_max, n=n_max)
+    def copy(self):
+        return MeshReTime(self.x_min, self.x_max, self.n)
 
 class MeshImFreq(Mesh_Generic):
     def __init__(self, beta, S, n_max=1025):
@@ -52,10 +65,15 @@ class MeshImFreq(Mesh_Generic):
             self.statistic = S
         else:
             raise ValueError("S must be 'Boson' or 'Fermion'.")
+        # Matsubara frequencies depend on whether we're bosons or fermions
+        # Boson = 0, Fermion = 1
         n = 2*n_max - 1 + statistic_enum.index(S)
         delta = 2*np.pi/beta
         xmax = complex(0,delta*(n_max-1) + statistic_enum.index(S)*delta/2)
         super().__init__(x_min = -xmax, x_max = xmax, n=n)
+    def copy(self):
+        n_max = self.last_index() + 1
+        return MeshImFreq(self.beta, self.statistic, n_max)
     def __call__(self, n):
         S = statistic_enum.index(self.statistic)
         return complex(0, (2*n + S) * np.pi / self.beta)
